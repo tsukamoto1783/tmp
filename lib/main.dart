b490953bef2d2b9a242a749b45efa5fa75383999
Tsukamoto Yuto <yuto.tsukamoto@ncdc.co.jp>
2024-11-07 15:33:18 +0900

    feat: webview_flutterのサンプル実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:webview_cookie_manager/webview_cookie_manager.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,16 +32,84 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('sample app'),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+      body: SafeArea(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const WebviewScreen(),
+                ),
+              );
+            },
+            child: const Text("webview 表示"),
+          ),
         ),
       ),
     );
   }
+}
+
+class WebviewScreen extends StatelessWidget {
+  const WebviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = _createController();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('sample app'),
+      ),
+      body: FutureBuilder(
+          future: controller,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return WebViewWidget(
+                controller: snapshot.data as WebViewController,
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
+    );
+  }
+}
+
+Future<WebViewController> _createController() async {
+  const demoUrl = 'https://flutter.dev';
+
+  final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) async {
+          final cookies = await WebviewCookieManager().getCookies(url);
+          print('getCookies: $cookies');
+        },
+        onHttpError: (HttpResponseError error) {},
+        onWebResourceError: (WebResourceError error) {},
+      ),
+    )
+    ..loadRequest(Uri.parse(demoUrl));
+
+  // cookieを設定
+  const cookie = WebViewCookie(
+    name: 'token',
+    value: 'your_access_token',
+    domain: demoUrl,
+  );
+  await WebViewCookieManager().setCookie(cookie);
+
+  // NOTE: 以下はandroidのみ有効。iosだと実行されない。
+  controller.runJavaScript("""
+  window.localStorage.setItem('access_token', 'your_access_token');
+  const token = window.localStorage.getItem('access_token');
+  window.confirm('取得したアクセストークン: ' + token);
+""");
+
+  return controller;
 }
